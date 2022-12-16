a1e2c60a02fcc201841271c95678ce68673b0fdb
bmtlima <bruno.makoto04@gmail.com>
2022-12-15 22:37:55 -0500

    bookmarks working, now gotta create folders and maybe stop using other bookmarks

--- page.js
+++ page.js
@@ -34,7 +34,7 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 $('#pgbutton').click(function() {
 
     console.log('javascript:(  function() %7B   window.open(%27' + taba.url + '%27)%3B   %7D  )()');
-    console.log('hero');
+    console.log(taba.url);
 
         //url: javascript:(  function() %7B   window.open(%27chrome://extensions/%27)%3B   %7D  )()
         //javascript:(  function() %7B   window.open(%27file:///Users/brunomakoto/Desktop/Books/Astro_Olimp_V2.pdf%27)%3B   %7D  )()
@@ -42,18 +42,23 @@ $('#pgbutton').click(function() {
 
         //javascript:(  function() %7B   window.open(%27file:///Users/brunomakoto/Desktop/Books/Astro_Olimp_V2.pdf%23page%3D300%27)%3B   %7D  )()
 
-    if (taba.url.includes('%23page%3D')){
-        let pos = text.indexOf('%23page%3D');
-        let pos2 = text.indexOf('%27)%3B'); 
+    if (taba.url.includes('#page=')){
+        console.log('cheguei fml');
+        let pos = taba.url.indexOf('#page=');
         //var pos = tab.url.indexOf('%23page%3D');
-        taba.url = taba.url.substring(0, pos) + taba.url.substring(pos2);
+        taba.url = taba.url.substring(0, pos);
+        console.log(taba.url);
     }
 
-    chrome.bookmarks.create({
-        title: book,
-        //url: "javascript:(  function() %7B   window.open(%27https://www.geeksforgeeks.org%27, %27_blank%27)%3B   %7D  )()"
-        url: 'javascript:(  function() %7B   window.open(%27' + taba.url + '%23page%3D' + page + '%27)%3B   %7D  )()',
-    }, callback());
+    function createTab() {
+        chrome.bookmarks.create({
+            title: book,
+            //url: "javascript:(  function() %7B   window.open(%27https://www.geeksforgeeks.org%27, %27_blank%27)%3B   %7D  )()"
+            url: 'javascript:(  function() %7B   window.open(%27' + taba.url + '%23page%3D' + page + '%27)%3B   %7D  )()',
+        }, callback());
+    };
+
+    createTab();
     
   });
 
