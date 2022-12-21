02821e492159ff3e1fe41fe88a4edaeddec83e97
bmtlima <bruno.makoto04@gmail.com>
2022-12-20 20:30:21 -0500

    all functionalities working, improve code now

--- page.js
+++ page.js
@@ -1,42 +1,23 @@
 
-/*$(function() {
-    $('#search').change(function() {
-       $('#bookmarks').empty();
-       dumpBookmarks($('#search').val());
-       console.log($('#search').val());
-    });
-  });
+/*
+* Bookmarks are arranged in a tree, where each node has characteristics such as
+* an unique id, a title, an url (unless the node is a folder), and information
+* about it's parent and children nodes. 
+*/
 
-  function dumpBookmarks(query) {
-    chrome.bookmarks.getTree(function(itemTree){
-        itemTree.forEach(function(item){
-            processNode(item);
-        });
-    });
-  }
-
-  function processNode(node) {
-    // recursively process child nodes
-    if(node.children) {
-        node.children.forEach(function(child) { processNode(child); });
-        //$('#bookmarks').append(dumpTreeNodes(processNode(node.children.forEach()), query));
-    }
-
-    // print leaf nodes URLs to console
-    if(node.url) { console.log(node); }
-    //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-}*/
-
-var idOne;
-var page = 0;
-var bookId;
-var bool = false;
-var book;
-var taba;
-var url;
-var arr = [];
+/*
+* Author: Bruno Lima
+*/
 
 
+
+var idOne; // id of the parent of the node that's going to be created. Equals 1 if 
+var page = 0; // page to be bookmarked
+var book; // name of the book to be bookmarked
+var taba; // current tab. Uses Chrome's tabs API
+var arr = []; // array with title and if of every folder 
+
+// Self executing function that calls pNode to recursively iterate through the bookmark tree
 (function dBookmarks() {
     console.log("book: " + book);
     chrome.bookmarks.getTree(function(itemTree){
@@ -46,76 +27,30 @@ var arr = [];
     });
 })();
 
+// Recursively process child nodes
 function pNode(node) {
-    // recursively process child nodes
-    if (arr.indexOf(node.title) == -1 && node.url == undefined){
+    if (arr.indexOf(node.title) == -1 && node.url == undefined){ // first condition prevents duplicates, second selects only folders
         arr.push(node.title);
         arr.push(node.id);
     }
     if(node.children) {
         node.children.forEach(
             function(child) {
-                if (child.title === "Bookmarks Bar"){
+                if (child.title === "Bookmarks Bar"){ // Getting id from the node whose children are the bookmarks we see 
                     idOne = child.id;
-                    //console.log("main: " + idOne);
-                }
-                
-                //console.log('child.title: ' + child.title + ' sohBook: ' + book);
-                if (child.title == book){
-                    bool = true;
-                    //console.log('deu bom, aq ta o book: ' + book + ' e aq ta o child.id: ' + child.id);
-                    bookId = child.id;
-                    //console.log(bookId);
-                    console.log('ale');
                 }
                 pNode(child); 
             }
         );
-        //$('#bookmarks').append(dumpTreeNodes(processNode(node.children.forEach()), query));
     }
 
-    // print leaf nodes URLs to console
-    if(node.url) { 
-        console.log(node); 
-    }
-    //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
+    // Useful for debugging: print leaf nodes URLs to console
+    //if(node.url) { 
+    //    console.log(node); 
+    //}
 }
 
-console.log(arr);
-
-function dBookmarks2(bookname){
-    chrome.bookmarks.getTree(function(itemTree){
-        itemTree.forEach(function(item){
-            pNode2(item, bookname);
-        });
-    });
-}
-
-function pNode2(node, namebook){
-    // recursively process child nodes
-    if(node.children) {
-        node.children.forEach(
-            function(child) {
-                if (child.title === namebook){
-                    idOne = child.id;
-                    console.log("idOne: " + idOne);
-                }
-                if (child.title == book){
-                    bookId = child.id;
-                }
-                pNode2(child, namebook); 
-            }
-        );
-    }
-
-    // print leaf nodes URLs to console
-    if(node.url) { 
-
-    }
-}
-
-//dBookmarks();
-
+//console.log(arr);
 
 // Making confirmation sign invisible
 var x = document.getElementById("confirmation");
@@ -139,56 +74,10 @@ function bookUpdater(bookInput) {
     book = bookInput;
 }
 
-/*
-console.log('a');
-//window.scrollTo(20000, 10000);
-setTimeout(function() {window.scrollTo(0, 1000);},1)
-console.log('b');*/
-
 // Update value of page when button is clicked
 function update(){
     x.style.display = "block";
     page = $('#pgnumber').val();
-    // AAA book = $('#bkname').val();
-    
-    //TESTANDO
-
-    /*chrome.bookmarks.getTree(function(itemTree){
-        itemTree.forEach(function(item){
-            console.log(item);
-            //pNode2(item);
-        });
-    });
-*/
-    /*
-    function pNode2(node) {
-        // recursively process child nodes
-        if(node.children) {
-            node.children.forEach(
-                function(child) {
-                    console.log('child.title: ' + child.title + ' sohBook: ' + book);
-                    if (child.title == book){
-                        bool = true;
-                        console.log('deu bom, aq ta o book: ' + book + ' e aq ta o child.id: ' + child.id);
-                        bookId = child.id;
-                        console.log(bookId);
-                    }
-                    pNode2(child); 
-                }
-            );
-            //$('#bookmarks').append(dumpTreeNodes(processNode(node.children.forEach()), query));
-        }
-    
-        // print leaf nodes URLs to console
-        if(node.url) { 
-            console.log(node); 
-        }
-        //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-    }
-
-    */
-
-    //TESTANDO
 }
 
 // Get the value of the current tab.
@@ -214,7 +103,6 @@ $('#pgbutton').click(function() {
 
     console.log('idOne before changing: ' + idOne);
     console.log(arr);
-    console.log()
     console.log('suposto valor do idOne: ' + arr[arr.indexOf(book) + 1]);
 
     function createTab(link) {
@@ -227,12 +115,9 @@ $('#pgbutton').click(function() {
             link = 'javascript:(  function() %7B   window.open(%27' + taba.url + '%23page%3D' + page + '%27)%3B   %7D  )()';
         }
 
-        if (arr.indexOf(book) != -1){
+        if (arr.indexOf(book) != -1 && page > 0){ // There's a folder with the book's name
             console.log('aaa');
             idOne = arr[arr.indexOf(book) + 1];
-        }
-
-        if(page > 0){
             chrome.bookmarks.create({
                 parentId: idOne,
                 title: 'Page ' + page,
@@ -240,7 +125,15 @@ $('#pgbutton').click(function() {
             }, callback());
         }
 
-        else{
+        if (arr.indexOf(book) == -1 && page > 0){
+            chrome.bookmarks.create({
+                parentId: idOne,
+                title: book + ' page ' + page,
+                url: link
+            }, callback());
+        }
+
+        if (!(page > 0)) {
             chrome.bookmarks.create({
             parentId: idOne,
             title: book,
@@ -255,11 +148,7 @@ $('#pgbutton').click(function() {
   });
 
   function callback() {
-    //window.location.reload();
+    window.location.reload();
   }
 
-  function callback2() {
-    
-  }
-
-console.log('last');
+  
--- popup.js
+++ popup.js
@@ -1,40 +1,4 @@
-/*var page;
 
-  $(function() {
-    $('#pageadd').change(function() {
-    page = $('#pageadd').val();
-    console.log(page);
-    });
-});
-
-  $( "form" ).submit(function( event ) {
-    if ( $( "input" ).first().val() === "correct" ) {
-      console.log('hi');
-    }
-   
-    $( "span" ).text( "Not valid!" ).show().fadeOut( 1000 );
-    event.preventDefault();
-  });*/
-
-  /*function dumpBookmarks(query) {
-    chrome.bookmarks.getTree(function(itemTree){
-        itemTree.forEach(function(item){
-            processNode(item);
-        });
-    });
-  }
-
-  function processNode(node) {
-    // recursively process child nodes
-    if(node.children) {
-        node.children.forEach(function(child) { processNode(child); });
-        //$('#bookmarks').append(dumpTreeNodes(processNode(node.children.forEach()), query));
-    }
-
-    // print leaf nodes URLs to console
-    if(node.url) { console.log(node.url); }
-    //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-}*/
 
 // Search the bookmarks when entering the search keyword.
 
@@ -45,33 +9,6 @@ $(function() {
        console.log($('#search').val());
     });
   });
-    
-//TESTE 
-
-/*
-  function dumpBookmarks(query) {
-    chrome.bookmarks.getTree(function(itemTree){
-        itemTree.forEach(function(item){
-            processNode(item);
-        });
-    });
-  }
-
-  function processNode(node) {
-    // recursively process child nodes
-    if(node.children) {
-        node.children.forEach(function(child) { processNode(child); });
-        //$('#bookmarks').append(dumpTreeNodes(processNode(node.children.forEach()), query));
-    }
-
-    // print leaf nodes URLs to console
-    if(node.url) { console.log(node); }
-    //$('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes, query));
-}
-*/
-
-// FIM DO TESTE
-
 
   // Traverse the bookmark tree, and print the folder and nodes.
   function dumpBookmarks(query) {
@@ -89,25 +26,12 @@ $(function() {
     var list = $('<ul>');
     var i;
     for (i = 0; i < bookmarkNodes.length; i++) {
-        // console.log('esse eh o tamanho de bookmarkNodes ' + bookmarkNodes.length);
-        //console.log(i + ' ' + bookmarkNodes[i].id);
-        /*if (bookmarkNodes[i].title === 'Bookmarks Bar' ){
-            dad = bookmarkNodes[i].id;
-            console.log(dad);
-        }*/
         list.append(dumpNode(bookmarkNodes[i], query));
     }
-    //console.log(list);
     return list;
   }
 
   function dumpNode(bookmarkNode, query) {
-    /*console.log(bookmarkNode);
-    if (bookmarkNode.title === "Bookmarks Bar"){
-        dad = bookmarkNode.id;
-        console.log('deu bom ' +  dad);
-        console.log(dad === 1);
-    }*/
     if (bookmarkNode.title) {
       if (query && !bookmarkNode.children) {
         if (String(bookmarkNode.title).indexOf(query) == -1) {
@@ -125,62 +49,22 @@ $(function() {
         chrome.tabs.create({url: bookmarkNode.url});
       });
       var span = $('<span>');
-      /*var options = bookmarkNode.children ?
-        $('<span>[<a href="#" id="addlink">Add</a>]</span>') :
-        $('<span>[<a id="deletelink" ' +
-          'href="#">Delete</a>]</span>');*/
       var options = $('<span>[<a id="deletelink" ' +
       'href="#">Delete</a>]</span>');
-      var edit = bookmarkNode.children ? $('<table><tr><td>Name</td><td>' +
-        '<input id="title"></td></tr><tr><td>URL</td><td><input id="url">' +
-        '</td></tr></table>') : $('<input>');
       
         // Show add and edit links when hover over.
           span.hover(function() {
           span.append(options);
           $('#deletelink').click(function() {
             chrome.bookmarks.remove(String(bookmarkNode.id));
+            chrome.bookmarks.removeTree(String(bookmarkNode.id));
             span.parent().remove();
+            if (children){
+              span.children().remove();
+            }
             $(this).dialog('destroy');
           });
 
-          $('#addlink').click(function() {
-            $('#adddialog').empty().append(edit).dialog({autoOpen: false,
-              closeOnEscape: true, title: 'Add New Bookmark', modal: true,
-              buttons: {
-              'Add' : function() {
-                 chrome.bookmarks.create({parentId: bookmarkNode.id,
-                   title: 'Book: ' + $('#title').val(), url: $('#url').val()});
-                 $('#bookmarks').empty();
-                 $(this).dialog('destroy');
-                 window.dumpBookmarks();
-               },
-              'Cancel': function() {
-                 $(this).dialog('destroy');
-              }
-            }}).dialog('open');
-          });
-
-
-
-          /*$('#editlink').click(function() {
-           edit.val(anchor.text());
-           $('#editdialog').empty().append(edit).dialog({autoOpen: false,
-             closeOnEscape: true, title: 'Edit Title', modal: true,
-             show: 'slide', buttons: {
-                'Save': function() {
-                   chrome.bookmarks.update(String(bookmarkNode.id), {
-                     title: edit.val()
-                   });
-                   anchor.text(edit.val());
-                   options.show();
-                   $(this).dialog('destroy');
-                },
-               'Cancel': function() {
-                   $(this).dialog('destroy');
-               }
-           }}).dialog('open');
-          });*/
           options.fadeIn();
         },
         // unhover
@@ -196,23 +80,6 @@ $(function() {
 
     return li;
 
-    //console.log(li[0].innerHTML);
-
-    /*const text = "style = 'display: none;'";
-    let inicio = li[0].innerHTML.substring(0,9);
-    let fim = li[0].innerHTML.substring(8);
-
-    if (li[0].innerHTML.includes('Ecolekl')){
-        li[0].innerHTML = inicio + text + fim;
-    }
-
-
-    if(li[0].innerHTML.includes('google')){
-        return li;
-    }
-    else {
-        return $('<span></span>');
-    }*/
   }
   
   document.addEventListener('DOMContentLoaded', function () {
